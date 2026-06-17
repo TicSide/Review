70ad920a485c22595df1b722c66a9a632b03bc87
Omar Harold Rivera Ticllacuri <66962537+TicSide@users.noreply.github.com>
2026-06-16 23:33:45 -0500

    Add ref parameter demos and helper methods

--- Review/Review/Program.cs
+++ Review/Review/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Review
 {
@@ -10,6 +7,63 @@ namespace Review
     {
         static void Main(string[] args)
         {
+            int numero = 5;
+            Duplicar(ref numero);
+            Console.WriteLine(numero);
+
+            string nombrePrueba = "Sauron";
+            Console.WriteLine("Su nombre de prueba es: " + nombrePrueba);
+            CambiarNombre(ref nombrePrueba);
+            Console.WriteLine(nombrePrueba);
+
+            decimal precioIva = 10.0m;
+            Console.WriteLine(precioIva + "%");
+            AgregarImpuesto(ref precioIva);
+            Console.WriteLine(precioIva + "%");
+
+            decimal price = 3800.0m;
+            Console.WriteLine("Se le hara un descuento del 10% al producto con: " + price);
+            AplicarDescuento(ref price);
+            Console.WriteLine("Con descuento seria: " + price);
+
+            string texto = "asdjlaksd";
+            Console.WriteLine(texto);
+            ConvertirAMayusculas(ref texto);
+            Console.WriteLine(texto);
+
+        }
+        public static void Duplicar(ref int numero)
+        {
+            numero *= 2;
+        }
+
+        public static string CambiarNombre(ref string nombre)
+        {
+            Console.WriteLine("Ponga el nombre que desee cambiarlo");
+            nombre = Console.ReadLine();
+            return nombre;
+        }
+        public static decimal AgregarImpuesto(ref decimal precio)
+        {
+            precio = 18.0m;
+            return precio;
+        }
+        public static int ReiniciarContador(ref int contador)
+        {
+            contador = 0;
+            return contador;
+        }
+        public static decimal AplicarDescuento(ref decimal total)
+        {
+            decimal discount=10m;
+            decimal finalTotal = 0;
+            total = total - (total * (discount/100));
+            return total;
+        }
+        public static string ConvertirAMayusculas(ref string texto)
+        {
+            texto=texto.ToUpper();
+            return texto;
         }
     }
 }
