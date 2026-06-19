eb050720130b5dde408a6b182357df7f56c67279
HaroldTicllacuri <66962537+TicSide@users.noreply.github.com>
2026-06-18 21:11:37 -0500

    Add Tryed class and invoke in Program

--- Review/Review/Program.cs
+++ Review/Review/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Review.TryCatch;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace Review
     {
         static void Main(string[] args)
         {
+            Tryed tryed= new Tryed();
+            tryed.Exercise1();
+
         }
     }
 }
---
+++ Review/Review/TryCatch/Tryed.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review.TryCatch
+{
+    public class Tryed
+    {
+        public void Exercise1()
+        {
+            int number1=0, number2=0;
+            int resultado = 0;
+            try {
+                Console.Write("Ingrese el numero 1: ");
+                number1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ingrese el numero 2: ");
+                number2 = Convert.ToInt32(Console.ReadLine());
+
+                resultado = number1 + number2;
+                Console.WriteLine(resultado);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ingrese un valor numero entero no decimal o textos");
+            }
+
+        } 
+
+    }
+}
