da01d4ec3e5bbf2967d29cc5c74fd48806db48ea
HaroldTicllacuri <66962537+TicSide@users.noreply.github.com>
2026-06-19 10:57:49 -0500

    Add ToDo CLI and update Program

--- Review/Review/Program.cs
+++ Review/Review/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Review
@@ -10,6 +11,60 @@ namespace Review
     {
         static void Main(string[] args)
         {
+            ToDo toDo=new ToDo();
+            int option;
+            try
+            {
+                do
+                {
+
+                    toDo.menuToDo();
+                    Console.ForegroundColor = ConsoleColor.White;
+                    option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                        default:
+                            break;
+                    }
+
+                } while (option > 0 && option < 6);
+            }
+            catch (FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("#####################");
+                Console.WriteLine("\tERROR");
+                Console.WriteLine("#####################");
+                Console.WriteLine("Ingrese un valor numerico entero (example: 1, 3, 4) Ni usar cadenas de texto");
+            }
+            finally
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Finalizar programa to-do en");
+                Console.ForegroundColor = ConsoleColor.White;
+                for (int i = 5; i >=0; i--)
+                {
+                    Console.Write(" "+i);
+                    Thread.Sleep(1500);
+                    if (i == 1)
+                    {
+                        Console.Clear();
+                        break;
+                    }
+                   
+                }
+            }
+
         }
     }
 }
---
+++ Review/Review/ToDo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Review
+{
+    internal class ToDo
+    {
+        public string path = "example.txt";
+        public string content = "Hello, World!\nThis is a new line of text.";
+        public void menuToDo()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("====================================");
+            Console.WriteLine("        TO-DO LIST POR CONSOLA");
+            Console.WriteLine("====================================");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("1. Ver tareas");
+            Console.WriteLine("2. Agregar tarea");
+            Console.WriteLine("3. Marcar tarea como completada");
+            Console.WriteLine("4. Eliminar tarea");
+            Console.WriteLine("5. Guardar y salir");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("Seleccione una opción: ");
+        }
+
+        public void menuWatchTasks()
+        {
+            Console.WriteLine("=========== LISTA DE TAREAS ===========");
+            Console.WriteLine("");
+            Console.WriteLine("ID   DESCRIPCIÓN              ESTADO        FECHA");
+            Console.WriteLine("---------------------------------------------------------");
+
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Presione ENTER para volver al menú...");
+            Console.Clear();
+        }
+        public void getInfoTasks()
+        {
+        }
+
+
+        public void menuAddTasks()
+        {
+            Console.WriteLine("=========== AGREGAR TAREA ===========");
+            Console.WriteLine("");
+            Console.WriteLine("Ingrese la descripción de la tarea:");
+            Console.Write("> ");
+            Console.WriteLine("");
+            Console.WriteLine("Tarea agregada correctamente.");
+            Console.WriteLine("");
+            Console.Write("ID: ");
+            Console.ReadLine();
+            Console.Write("Descripción: ");
+            Console.ReadLine();
+            Console.Write("Estado: ");
+            Console.ReadLine();
+            Console.Write("Fecha: ");
+            Console.ReadLine();
+        }
+
+        public void menuCompleteTasks()
+        {
+            Console.WriteLine("====== MARCAR TAREA COMO COMPLETADA ======");
+            Console.WriteLine();
+            Console.WriteLine("Ingrese el ID de la tarea: ");
+            Console.Write("> ");
+            Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine(" La tarea {0} fue marcada como completada.");
+        }
+
+        public void menuRemoveTasks()
+        {
+            Console.WriteLine("=========== ELIMINAR TAREA ===========");
+            Console.WriteLine();
+            Console.WriteLine("Ingrese el ID de la tarea que desea eliminar: ");
+            Console.Write("> ");
+            Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine(" La tarea {0} fue eliminada correctamente.");
+        }
+    }
+}
